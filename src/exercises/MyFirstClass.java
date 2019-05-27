ebb37940181f02ff35df7849d5acaa8bfdd7a2f9
martijnvanes <51001282+martijnvanes@users.noreply.github.com>
2019-05-27 22:22:42 +0200

    babySteps

---
+++ src/exercises/MyFirstClass.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class MyFirstClass {
+    public static void main(String[] args) {
+        byte aByteValue = 1;
+        System.out.println("Tester number " + aByteValue);
+
+    }
+}
