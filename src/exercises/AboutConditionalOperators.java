6dc0c87d6c7235c931b0245d775d6a8695c2190f
martijnvanes <51001282+martijnvanes@users.noreply.github.com>
2019-06-05 22:41:10 +0200

    AboutConditionalOperators

---
+++ src/exercises/AboutConditionalOperators.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class AboutConditionalOperators {
+    public static void main(String[] args) {
+        System.out.println(getExamResult(100));
+        System.out.println(getExamResult(99));
+        System.out.println(getExamResult(65));
+    }
+    private static String getExamResult(int score) {
+        if (score > 99) {
+            return "Well done!";
+        }
+        if (score <= 65) {
+            return "Too bad, you failed";
+        }
+        else {
+            return "You passed";
+        }
+    }
+
+
+}
